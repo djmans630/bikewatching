5c9518604f95002c8bf0c5fdfd6518ac787c9e15
djmans630 <dmansperger@ucsd.edu>
2025-02-20 18:47:07 -0800

    part of 1.3

---
+++ map.js
@@ -0,0 +1,11 @@
+mapboxgl.accessToken = 'YOUR_ACCESS_TOKEN_HERE';
+
+   // Initialize the map
+   const map = new mapboxgl.Map({
+     container: 'map', // ID of the div where the map will render
+     style: 'mapbox://styles/mapbox/streets-v12', // Map style
+     center: [-71.09415, 42.36027], // [longitude, latitude]
+     zoom: 12, // Initial zoom level
+     minZoom: 5, // Minimum allowed zoom
+     maxZoom: 18 // Maximum allowed zoom
+   });
