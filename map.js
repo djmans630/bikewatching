7e9ae534f441855f9c79b48eb06b1331f23d8730
djmans630 <dmansperger@ucsd.edu>
2025-02-20 19:27:56 -0800

    2.2

--- map.js
+++ map.js
@@ -9,3 +9,20 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZGptYW5zNjMwIiwiYSI6ImNtN2U1eWQ5eDBiOGsybnB2N
      minZoom: 5, // Minimum allowed zoom
      maxZoom: 18 // Maximum allowed zoom
    });
+
+   map.on('load', () => { 
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?...'
+      });
+      map.addLayer({
+        id: 'bike-lanes',
+        type: 'line',
+        source: 'boston_route',
+        paint: {
+            'line-color': '#32D400',  // A bright green using hex code
+            'line-width': 5,          // Thicker lines
+            'line-opacity': 0.6       // Slightly less transparent
+          }
+      });
+  });
